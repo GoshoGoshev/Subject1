2e8cd7dcb571acb88aa2984db7cbd2131989244c
GoshoGoshev <dimitar.genov17@abv.bg>
2019-02-02 17:18:31 +0200

    zad2

---
+++ Binary Sorcceres LV 2/Binary Sorcceres LV 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+            double S=0;
+            int n=0;
+            double Step;
+            while (a != 0)
+            {
+                
+                Step=Math.Pow(2,n);
+                S = S + a % 10 * Step;
+                a = a / 10;
+                n = n+1;
+            }
+            Console.WriteLine(S);
+
+
+        }
+    }
+}
