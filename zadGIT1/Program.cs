5006dec6c03504f2ceeaaa13715c4347b006226b
GoshoGoshev <dimitar.genov17@abv.bg>
2019-02-02 13:32:49 +0200

    aide gotovi sme

---
+++ zadGIT1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+          
+
+            Console.WriteLine("Godini=" + a*100);
+            Console.WriteLine("Dni=" + a * 100*365);
+            Console.WriteLine("Chasove=" + a * 100 * 365*24);
+            Console.WriteLine("Minuti=" + a * 100 * 365 * 24*60);
+           
+        }
+    }
+}
