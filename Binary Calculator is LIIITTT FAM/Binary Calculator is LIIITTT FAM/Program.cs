0d08308e0a0863c2000e963681806756d9a522f3
GoshoGoshev <dimitar.genov17@abv.bg>
2019-02-02 17:04:36 +0200

    1zad

---
+++ Binary Calculator is LIIITTT FAM/Binary Calculator is LIIITTT FAM/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+       
+            long S = 0;
+            long n = 1;
+
+                while (a != 0)
+                {
+                    S = S + a % 2 * n;
+                    n = n * 10;
+                    a = a / 2;
+                }
+                Console.WriteLine(S);
+        }
+    }
+}
