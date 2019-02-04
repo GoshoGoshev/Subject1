f61e8b69089ac5c03a7466a791c370b03486ab21
GoshoGoshev <dimitar.genov17@abv.bg>
2019-02-04 13:22:31 +0200

    nenujna zad

---
+++ char/char/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a =char.Parse(Console.ReadLine());
+            var b = char.Parse(Console.ReadLine());
+            var c = char.Parse(Console.ReadLine());
+            Console.WriteLine("{0}{1}{2}" , c,b,a);
+        }
+    }
+}
