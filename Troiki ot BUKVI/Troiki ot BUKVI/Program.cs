1f4b106f84d0f17e8ed5e4803629ee29ddb919c5
GoshoGoshev <dimitar.genov17@abv.bg>
2019-02-04 12:40:10 +0200

    zad8

---
+++ Troiki ot BUKVI/Troiki ot BUKVI/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var n = int.Parse(Console.ReadLine());
+            char z1='a',z2='a',z3='a';
+            for (int i = 1; i <= n; i++)
+            { 
+                for (int j = 1; j <= n; j++)
+                {
+                    
+                    for (int k = 1; k <= n; k++)
+                    {
+                       
+                        Console.Write(z1);
+                        Console.Write(z2);
+                        Console.Write(z3);
+                        Console.WriteLine();
+                        z3++; if (k == n) z3 = 'a';
+                    }
+                    z2++;if (j == n) z2 = 'a';
+                }
+                 z1++;
+            }
+        }
+    }
+}
