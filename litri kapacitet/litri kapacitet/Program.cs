c937118ed04b57ae409515c10126db23fec6556a
GoshoGoshev <dimitar.genov17@abv.bg>
2019-02-04 13:36:25 +0200

    zad11

---
+++ litri kapacitet/litri kapacitet/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var n = int.Parse(Console.ReadLine());
+            int S=0;
+            for (int i = 0; i < n; i++)
+            {
+                var a = int.Parse(Console.ReadLine());
+                S = S + a;
+                if (S > 255) { Console.WriteLine("Insufficient capacity!"); S = S - a; }
+            }
+            Console.WriteLine(S);
+        }
+    }
+}
