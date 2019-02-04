2e3746599a62e2323cbaf7cf776d07b0f642bdf1
GoshoGoshev <dimitar.genov17@abv.bg>
2019-02-04 12:47:06 +0200

    zad9

---
+++ Info/Info/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fname = Console.ReadLine();
+            var sname = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hello, {0} {1}. You are {2} years old." , fname,sname,age);
+        }
+    }
+}
