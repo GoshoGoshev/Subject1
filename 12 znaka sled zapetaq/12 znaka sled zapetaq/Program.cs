5ce785d28f8ca18511ced13ecb2ba625e7fc82e4
GoshoGoshev <dimitar.genov17@abv.bg>
2019-02-04 11:07:22 +0200

    zad5

---
+++ 12 znaka sled zapetaq/12 znaka sled zapetaq/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = double.Parse(Console.ReadLine());
+            Console.WriteLine("{0:f12}", Math.PI*r*r);
+        }
+    }
+}
