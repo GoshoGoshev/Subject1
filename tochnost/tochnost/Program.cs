b00c595de63de4c7423ea303e171ef8bb55ea6d2
GoshoGoshev <dimitar.genov17@abv.bg>
2019-02-04 11:23:22 +0200

    zad6

---
+++ tochnost/tochnost/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var n = int.Parse(Console.ReadLine());
+            decimal sum = 0;
+            for (int i = 0; i < n; i++)
+                sum += decimal.Parse(Console.ReadLine());
+            Console.WriteLine(sum);
+
+
+        }
+    }
+}
