0bcdea159c86ff085418b29ec6f53d20db50fa7c
GoshoGoshev <dimitar.genov17@abv.bg>
2019-02-04 09:50:54 +0200

    oshte nqkolko zadachi

---
+++ Suma ot ostatuci/Suma ot ostatuci/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = int.Parse(Console.ReadLine());
+            int S=0;
+            for (int i = 0; i < c; i++)
+            {
+                var a = int.Parse(Console.ReadLine());
+                var b = int.Parse(Console.ReadLine());
+                S = S + a % b;
+            }
+            Console.WriteLine("Sum ot ostatuci:" + S);
+        }
+    }
+}
