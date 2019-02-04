a44bd133f1e927e99a08b803e7866555ce4d6327
GoshoGoshev <dimitar.genov17@abv.bg>
2019-02-04 10:06:40 +0200

    zad4

---
+++ ot DEC kum HEX/ot DEC kum HEX/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+            var d = Convert.ToString(a, 16);
+            Console.WriteLine(d.ToUpper());
+        }
+    }
+}
