6b8f0aedb28df04a25f0c75efe145e7808b1c0df
GoshoGoshev <dimitar.genov17@abv.bg>
2019-02-04 12:15:25 +0200

    zad7

---
+++ ConvertNIZ/ConvertNIZ/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var a = Console.ReadLine();
+            var b = Convert.ToBoolean(a);
+            if (b == true) Console.WriteLine("Yes");
+            else Console.WriteLine("No");
+
+        }
+    }
+}
