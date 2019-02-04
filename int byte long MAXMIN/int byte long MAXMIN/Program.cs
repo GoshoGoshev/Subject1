a05bdec6b37e04ba2cdbedaacf6076e6dca03322
GoshoGoshev <dimitar.genov17@abv.bg>
2019-02-04 13:21:00 +0200

    zad10

---
+++ int byte long MAXMIN/int byte long MAXMIN/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = Console.ReadLine();
+            if (a == "int") { Console.WriteLine(int.MaxValue); Console.WriteLine(int.MinValue); }
+            if (a == "uint") { Console.WriteLine(uint.MaxValue); Console.WriteLine(uint.MinValue); }
+            if (a == "long") { Console.WriteLine(long.MaxValue); Console.WriteLine(long.MinValue); }
+            if (a == "byte") { Console.WriteLine(byte.MaxValue); Console.WriteLine(byte.MinValue); }
+            if (a == "sbyte") { Console.WriteLine(sbyte.MaxValue); Console.WriteLine(sbyte.MinValue); }
+        }
+    }
+}
