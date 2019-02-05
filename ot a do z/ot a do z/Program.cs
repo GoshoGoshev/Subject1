2b4200c3d7f5de94771f1ac8b02ce4f01b6159fa
GoshoGoshev <dimitar.genov17@abv.bg>
2019-02-05 14:15:38 +0200

    zad14

---
+++ ot a do z/ot a do z/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = Console.ReadLine();
+            for(int i=0;i<a.Length;i++)if(a[i]>='a'&&a[i]<='z')Console.WriteLine("{0}-{1} ",a[i],(int)a[i]-96);
+
+        }
+    }
+}
