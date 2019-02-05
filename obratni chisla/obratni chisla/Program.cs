0059dd19f6018cb60e9e1e6aeecde58ff8d6c21f
GoshoGoshev <dimitar.genov17@abv.bg>
2019-02-05 14:16:33 +0200

    15zad

---
+++ obratni chisla/obratni chisla/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n=int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            for (int i = 0; i < a.Length; i++) a[i] = int.Parse(Console.ReadLine());
+            for (int j = a.Length-1; j >= 0; j--) Console.WriteLine(a[j]);
+        }
+    }
+}
