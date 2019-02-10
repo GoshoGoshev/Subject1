5df288b350b30f7fbbf55b24f2770491418a3110
GoshoGoshev <dimitar.genov17@abv.bg>
2019-02-10 14:21:34 +0200

    zad15

---
+++ 1 Spisak/1 Spisak/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var a = new List<int> { };
+            for(int i=0;i<n;i++)a.Add(int.Parse(Console.ReadLine()));
+            for(int i=0;i<a.Count;i++)if(a[i]%2==0)Console.Write(a[i]+" ");
+        }
+    }
+}
