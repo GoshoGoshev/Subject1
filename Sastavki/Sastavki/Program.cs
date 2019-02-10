4ef6f6b291c871334683aff6b2c328744acbbc72
GoshoGoshev <dimitar.genov17@abv.bg>
2019-02-10 14:22:16 +0200

    zad16

---
+++ Sastavki/Sastavki/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var a = new List<string>();
+            for (int i = 0; i < n; i++) a.Add(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+            int br=0;
+            for (int i = 0; i < n; i++)
+            {
+                if(a[i].Length==b) Console.WriteLine(a[i]);br++;
+                if (br > 10) break;
+            }
+            
+
+        }
+    }
+}
