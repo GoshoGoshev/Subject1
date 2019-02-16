0d3fe6315eea8585576a01a6ecddd2abaa376a9b
GoshoGoshev <dimitar.genov17@abv.bg>
2019-02-16 13:28:58 +0200

    23zad

---
+++ Dvumerni masivi/Dvumerni masivi/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = new int[3, 3];
+            for (int row = 0; row < a.GetLength(0); row++)
+            {
+                for (int col = 0; col < a.GetLength(1); col++)
+                {
+                    a[row, col] = int.Parse(Console.ReadLine());
+                }
+            }
+            for (int row = 0; row < a.GetLength(0); row++)
+            {
+                for (int col = 0; col < a.GetLength(1); col++)
+                {
+                    Console.Write(a[row,col]+" ");//a[row, col] = int.Parse(Console.ReadLine());
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            for (int row = 0; row < a.GetLength(0); row++)
+            {
+                for (int col = 0; col < a.GetLength(1); col++)
+                {
+                    if(row%2==0)Console.Write(a[row, col]+" ");
+                    //a[row, col] = int.Parse(Console.ReadLine());
+                }
+            }  Console.WriteLine();
+        }
+    }
+}
