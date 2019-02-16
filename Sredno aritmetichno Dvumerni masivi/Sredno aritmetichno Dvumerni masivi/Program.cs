6add8d569f707650018cd9b19e11198c887135c3
GoshoGoshev <dimitar.genov17@abv.bg>
2019-02-16 13:27:26 +0200

    20zad

---
+++ Sredno aritmetichno Dvumerni masivi/Sredno aritmetichno Dvumerni masivi/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var b = int.Parse(Console.ReadLine());
+            var c = int.Parse(Console.ReadLine());
+            int[,] a = new int[b, c];
+            for (int row = 0; row < a.GetLength(0); row++)
+            {
+
+                for (int col = 0; col < a.GetLength(1); col++)
+                {
+                    a[row, col] = int.Parse(Console.ReadLine());
+                }
+            }
+            
+            for (int row = 0; row < a.GetLength(0); row++)
+            {
+                double sr=0;
+                for (int col = 0; col < a.GetLength(1); col++)
+                {
+                    sr = sr + a[row, col];
+                   Console.Write(a[row, col]+" "); 
+                }
+                Console.Write(sr/a.GetLength(1));Console.WriteLine();
+            }
+        }
+            
+    }
+}
