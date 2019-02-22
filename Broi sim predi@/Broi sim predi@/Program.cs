616c49744a31ed2e7ccf1714a79163697def9a42
GoshoGoshev <dimitar.genov17@abv.bg>
2019-02-22 16:29:40 +0200

    bez String builder

---
+++ Broi sim predi@/Broi sim predi@/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = Console.ReadLine();
+            int sz = a.Length;
+            a.ToCharArray();
+            int brpr = 0;
+            int krai=0;
+            for(int i=0;i<sz;i++)
+            {
+                brpr++;
+                if (a[i] == '@') krai = brpr;
+                }
+            if (brpr < 2 * krai) Console.WriteLine("Call her!");
+            else Console.WriteLine("She is not the one D:");
+
+        }
+    }
+}
