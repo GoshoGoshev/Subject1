2c468f43afa41c40b512225add95e595787a7e26
GoshoGoshev <dimitar.genov17@abv.bg>
2019-02-22 16:31:17 +0200

    Fast af

---
+++ Palindrom na CHARarray/Palindrom na CHARarray/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = Console.ReadLine();
+            int abr = a.Length;
+            a.ToCharArray();
+            char[] b = new char[a.Length];
+            int k=0,br=0;
+            for (int i = abr - 1; i >= 0; i--) { b[k] = a[i]; k++; }
+            for (int i = 0; i < abr; i++) if (b[i] == a[i]) br++;
+            if (br == abr) Console.WriteLine("True");
+            else Console.WriteLine("False");
+        }
+    }
+}
