a7106673839bdc0edf13f86c0203a1b51214443d
GoshoGoshev <dimitar.genov17@abv.bg>
2019-03-09 12:16:58 +0200

    Same

---
+++ Vgradeni/Vgradeni/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Sum={0}", a.Sum());
+            Console.WriteLine("Min={0}", a.Min());
+            Console.WriteLine("Max={0}", a.Max());
+            Console.WriteLine("Srednoar={0}", a.Average());
+        }
+    }
+}
