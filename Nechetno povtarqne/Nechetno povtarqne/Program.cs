e7454a06204ffb724049df61e03ff5e88f0b54b0
GoshoGoshev <dimitar.genov17@abv.bg>
2019-03-09 12:16:21 +0200

    Selfexplanetory

---
+++ Nechetno povtarqne/Nechetno povtarqne/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string b = Console.ReadLine().ToLower();
+            string[] a = b.Split(' ');
+            var counts = new Dictionary<string, int>();
+            foreach (var check in a)
+                if (counts.ContainsKey(check))
+                    counts[check]++;
+                else counts[check] = 1;
+            var results = new List<string>();
+
+            foreach (var pair in counts)
+            {
+                if (pair.Value % 2 == 1) results.Add(pair.Key);
+
+            }
+                Console.WriteLine(string.Join(", ", results));
+
+
+        }
+    }
+}
