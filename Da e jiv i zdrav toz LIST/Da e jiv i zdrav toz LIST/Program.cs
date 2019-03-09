85c0aeba8d1c7e1a546f7dcdcf871c0a8136641c
GoshoGoshev <dimitar.genov17@abv.bg>
2019-03-09 12:13:14 +0200

    Nikva ideq ko prai taz zad

---
+++ Da e jiv i zdrav toz LIST/Da e jiv i zdrav toz LIST/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d=new Dictionary<string,string>();
+            var result=new List<string>();
+           // string b;
+            for (int i = 0; i < 3; i++)
+            {
+                var b = Console.ReadLine();
+                string[] a = b.Split(' ');
+                int k = a.Length;
+                //Console.WriteLine(c[0]);
+                if (a[0] == "A") d.Add(a[1], a[k - 1]);
+                if (a[0] == "S") foreach (var check in d) if (check.Key == a[1])  result.Add(check.Key+"->"+check.Value); 
+                        else result.Add("Invalid number");
+            }
+            foreach (var p in result)
+            {
+                //if (p == "Invalid number") { Console.WriteLine(); 
+                Console.WriteLine(p);
+                //else Console.Write(p + "->");
+            } Console.WriteLine();
+        }
+    }
+}
